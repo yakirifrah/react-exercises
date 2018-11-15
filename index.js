b09c6a88bb4f0a0c56b5175cf2c5aeac38d3ec95
lotemh <lotem.h@elasticmedia.io>
2018-11-15 17:32:29 +0200

    template for react with server

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lotem on 1/23/2016.
+ */
+
+ var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 7000));
+
+app.use(express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+    console.log('Open your browser with localhost:', app.get('port'));
+});
