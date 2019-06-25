f3365007d938896231e8b19f967426eb4e02cf38
yakir <yakirifrah19@gmail.com>
2019-06-25 10:45:17 +0300

    added ex1 class - props

---
+++ d2_JSX/start/ex2/logic.js
@@ -0,0 +1,209 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <Nav />
+            </div>
+        );
+    }
+}
+class StripeCenter extends React.Component {
+    constructor() {
+        super();
+    }
+    onEnter(event) {
+        $(event.target).toggleClass('change-background-icon');
+    }
+    onLeave(event) {
+     
+
+
+    }
+    render() {
+        return (
+            <div className="stripe-center">
+                <StripeIcon1 onEnter={this.onEnter} onLeave={this.onLeave} />
+                <StripeIcon2 onEnter={this.onEnter} onLeave={this.onLeave} />
+                <StripeIcon3 onEnter={this.onEnter} onLeave={this.onLeave} />
+            </div>
+        );
+    }
+}
+class StripeIcon1 extends React.Component {
+    render() {
+        return (
+            <div className="stripe-icon">
+                <div onMouseEnter={this.props.onEnter} onMouseLeave={this.props.onLeave} className="icon1">
+                </div>
+                <TitleIcon1 />
+                <SecondaryTitleIcon1 />
+            </div>
+        );
+    }
+}
+class TitleIcon1 extends React.Component {
+    render() {
+        return (
+            <div className="title-icon">USER INRTERFACE DESIGN</div>
+        );
+    }
+}
+class SecondaryTitleIcon1 extends React.Component {
+    render() {
+        return (
+            <ul className="main-ul">
+                <li className="main-li">
+                    Wireframing
+                </li>
+                <li className="main-li">
+                    Prototyping
+                </li>
+                <li className="main-li">
+                    Usabillity testing
+                </li>
+            </ul>
+        );
+    }
+}
+
+class StripeIcon2 extends React.Component {
+    render() {
+        return (
+            <div className="stripe-icon">
+                <div onMouseEnter={this.props.onEnter} onMouseLeave={this.props.onLeave} className="icon2">
+                </div>
+                <TitleIcon2 />
+                <SecondaryTitleIcon2 />
+            </div>
+        );
+    }
+}
+class TitleIcon2 extends React.Component {
+    render() {
+        return (
+            <div className="title-icon">CONCEPT AND IDEAS</div>
+        );
+    }
+}
+class SecondaryTitleIcon2 extends React.Component {
+    render() {
+        return (
+            <ul className="main-ul">
+                <li className="main-li">
+                    Conceptuallazation
+                </li>
+                <li className="main-li">
+                    Digital Branding
+                </li>
+                <li className="main-li">
+                    Product Strategy
+                </li>
+            </ul>
+        );
+    }
+}
+
+class StripeIcon3 extends React.Component {
+    render() {
+        return (
+            <div className="stripe-icon">
+                <div onMouseEnter={this.props.onEnter} onMouseLeave={this.props.onLeave} className="icon3">
+                </div>
+                <TitleIcon3 />
+                <SecondaryTitleIcon3 />
+            </div>
+        );
+    }
+}
+class TitleIcon3 extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="title-icon">DESIGN END BRANDING</div>
+            </div>
+        )
+    };
+}
+class SecondaryTitleIcon3 extends React.Component {
+    render() {
+        return (
+            <ul className="main-ul">
+                <li className="main-li">
+                    Interection Design
+                </li>
+                <li className="main-li">
+                    Graphic Design
+                </li>
+                <li className="main-li">
+                    Indentity Design
+                </li>
+            </ul>
+        );
+    }
+}
+
+class MainTitle extends React.Component {
+    render() {
+        return (
+            <div className="title-main">
+                SERVICES
+            </div>
+        );
+    }
+}
+class DescriptionTitle extends React.Component {
+    render() {
+        return (
+            <div className="desciption-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis a reiciendis repellat molestias quod corrupti fugiat numquam quibusdam ex sapiente aperiam itaque architecto sit rerum, expedita iure esse? Magni, nam.</p>
+            </div>
+        );
+    }
+}
+class GroupTitle extends React.Component {
+    render() {
+        return (
+            <div className="group-title">
+                <MainTitle />
+                <DescriptionTitle />
+            </div>
+        );
+    }
+}
+class Nav extends React.Component {
+    render() {
+        return (
+            <ul className="nav">
+                <li><a href="">SERVICES</a></li>
+                <li><a href="">WORK</a></li>
+                <li><a href="">THE TEAM</a></li>
+                <li className="last-item"><img src="https://imageog.flaticon.com/icons/png/512/35/35432.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" /></li>
+            </ul>
+        );
+    }
+}
+class Main extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                <GroupTitle />
+                <StripeCenter />
+            </div>
+        );
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Main />
+            </div>
+
+        );
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
---
+++ d3_props/class/ex1/logic.js
@@ -0,0 +1,23 @@
+const Title = ({ font, text, size }) => {
+    let style = {
+        fontFamily: font,
+        fontSize: size,
+    }
+    return (
+        <h1 style={style}>{text}</h1>
+    );
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Title font="Times New Roman" size="20px" text="if I Rule the DOM" />
+                <Title font="Ariel" size="40px" text="Talk is cheap.Show me the code" />
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
