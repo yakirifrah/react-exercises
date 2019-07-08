d76d553653ebf29ecaa7860584902b980820d460
yakir <yakirifrah19@gmail.com>
2019-07-08 21:40:10 +0300

    ex3 react state vs props

--- d4_state/start/ex3/logic.js
+++ d4_state/start/ex3/logic.js
@@ -1,28 +1,39 @@
 const App = () => {
     return (
         <div>
-            <TextVisableInProps visable={true} />
+            <TextVisableInProps isVisable={true} />
+            <TextVisableInProps isVisable={false} />
             <TextVisableInState />
         </div>);
 }
 
 class TextVisableInProps extends React.Component {
-
-
-    click() {
-        
-    }
     render() {
+        const { isVisable } = this.props;
+        let isVisablity = isVisable ? "block" : "none";
         return (
-            <h1 onClick={this.click}>{}</h1>
+            <div>
+                <h1>text visbility with props</h1>
+                <h2>the text is visable:{isVisable.toString()}</h2>
+                <h3 style={{ display: isVisablity }}>the text is visbilty</h3>
+            </div>
+        );
 
-        )
     }
-
 }
 class TextVisableInState extends React.Component {
     constructor() {
         super();
+        this.state = ({ isVisable: true });
+    }
+    render() {
+        let isVisablity = this.state.isVisable ? "block" : "none"
+        return (
+            <div onMouseEnter={() => this.setState({ isVisable: !this.state.isVisable })}>
+                <h2 onClick={() => this.setState({ isVisable: !this.state.isVisable })}>text is visablity with state</h2>
+                <h2 style={{ display: isVisablity }}>visable</h2>
+            </div>
+        )
     }
 }
 
